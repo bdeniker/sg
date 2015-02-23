8f5cdb878ef5dcdc5aa37a9f6c2810ee5f334866
sienf <sienfae@gmail.com>
2015-02-23 00:20:03 +0000

    Setup for hand generation

--- cards/urls.py
+++ cards/urls.py
@@ -6,4 +6,5 @@ from cards import views
 urlpatterns = patterns('',
     url(r'^$', SkillList.as_view(), name='skill-index'),
     url(r'^(?P<skill_name>[a-zA-Z ]+)/$', views.skill_view, name='skill'),
+    url(r'^(?P<skill_name>[a-zA-Z ]+)/card-hands$', views.card_hands_view, name='card-hands'),
 )
--- cards/views.py
+++ cards/views.py
@@ -14,3 +14,10 @@ def skill_view(request, skill_name):
                   {'skill': skill,
                    'problems': skill.problem_set.annotate(card_count=Count('card')).order_by('card_count'),
                    'cards': skill.card_set.order_by('duration')})
+
+def card_hands_view(request, skill_name):
+    skill = get_object_or_404(Skill, name=skill_name)
+    cards = int(request.GET.get('cards', 6))
+    hands = int(request.GET.get('hands', 5))
+    return render(request, 'cards/card_hands_view.html',
+                  {'skill': skill, 'cards': cards, 'hands': hands})
