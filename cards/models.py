7507ae6515e8f5cbc5d38810a5eb1a4b9d74a811
Bobbie Sinkova <boryana.sinkova@cgi.com>
2015-02-23 17:25:10 +0000

    Add Hand model and listen to signals that would invalidate generated hands

--- cards/models.py
+++ cards/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.db.models import Q
 
+# NOTE Signals captured in apps.py!
+
 # Natural key managers
 class SkillManager(models.Manager):
     def get_by_natural_key(self, name):
@@ -56,3 +58,30 @@ class Card(models.Model):
 
     def problems_solved_count(self):
         return self.problems.all().count()
+
+class Hand(models.Model):
+    skill = models.ForeignKey(Skill)
+    cards = models.ManyToManyField(Card)
+    coverage = models.DecimalField()
+    redundancy = models.DecimalField()
+
+    def generate_stats(self):
+        # For each problem for this skill, how many cards in this hand cover it
+        problems = 0
+        coverage = 0
+        redundancy = 0
+
+        for problem in Problem.objects.filter(skill=self.skill):
+            problems += 1
+            cards_solving = Card.objects.filter(
+                problems__id__exact=problem.id).count()
+            if cards_solving > 1:
+                coverage += 1
+            redundancy += cards_solving
+
+        return (coverage/problems, redundancy/problems)
+
+    def save(self):
+        super(Hand, self).save()
+        self.coverage, self.redundancy = self.generate_stats()
+        super(Hand, self).save()
